443cbd5f47c11afa6337e7d1ab614a11093d55ee
Bobby Gifford <bobby077joe@gmail.com>
2020-07-15 15:38:56 -0700

    Initialize likes with 0 on post Create.

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.create(
+        { ...data, likes: 0 },
+        { files }
+      );
+    } else {
+      entity = await strapi.services.post.create({
+        ...ctx.request.body,
+        likes: 0,
+      });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
