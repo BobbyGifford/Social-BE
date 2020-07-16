5d0c518b7d10054517afd6a376cb5219a3ad9a70
Bobby Gifford <bobby077joe@gmail.com>
2020-07-16 14:33:56 -0700

    Refined errors

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -12,15 +12,21 @@ module.exports = {
     let entity;
     if (ctx.is("multipart")) {
       const { data, files } = parseMultipartData(ctx);
+
+      if (!data || !data.description) {
+        ctx.throw(400, "Please add description");
+      }
+
+      if (!files || !files.image) {
+        ctx.throw(400, "Please add an image");
+      }
+
       entity = await strapi.services.post.create(
         { ...data, likes: 0 },
         { files }
       );
     } else {
-      entity = await strapi.services.post.create({
-        ...ctx.request.body,
-        likes: 0,
-      });
+      ctx.throw(400, "Please submit required form data");
     }
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
